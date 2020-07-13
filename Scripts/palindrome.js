24c4a3f95cd41d0172c7ac0a08c8b10d7fb5f3de
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-07-13 07:41:43 -0700

    Initial commit

---
+++ Scripts/palindrome.js
@@ -0,0 +1,25 @@
+﻿
+$("#submitButton").click(function () {
+             
+    let removeNonAlphasAndNumerics = /[\W_]/g;
+    let phrase = $("#phrase").val();
+
+    let cleanPhrase = phrase.toLowerCase().replace(removeNonAlphasAndNumerics, "");
+
+    let reversedPhrase = cleanPhrase.split("").reverse().join("");
+    $("#resultText").val(reversedPhrase);   
+
+    if (cleanPhrase === reversedPhrase) {
+        $("#title").text("palindrome!");
+    } else {
+        $("#title").text("!palindrome");
+    }
+});
+
+$("#clearButton").on("click", function () {
+    $("#phrase").val("").focus();
+    $("#resultText").val("");  
+    $("#title").text("Palindrome");
+});
+
+
